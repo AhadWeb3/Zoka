cd5c3a66e6d889355275e2cee5249c8e9c7936af
Abdul Ahad <ahadweb3.0@gmail.com>
2022-11-07 20:42:17 +0530

    background image

---
+++ components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+        <div className='custom-image w-full h-screen bg-no-repeat bg-center bg-cover'>
+
+        <div className=" bg-black/50 absolute top-0 bottom-0 right-0 left-0 z-10"></div>
+        <div>Heading</div>
+        <div>Message</div>
+        <div>Book</div>
+        </div>
+  )
+}
+
+export default Hero
